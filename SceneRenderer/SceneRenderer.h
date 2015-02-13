0451cdeb8b5636ab8d4cf98943ee230a84cbd4df
Galia Kaufman <gkaufman@redbrickhealth.com>
2015-02-13 12:08:30 -0500

    Initial Commit

---
+++ SceneRenderer/SceneRenderer.h
@@ -0,0 +1,19 @@
+//
+//  SceneRenderer.h
+//  SceneRenderer
+//
+//  Created by Galia Kaufman on 2/13/15.
+//  Copyright (c) 2015 Redbrick Health. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SceneRenderer.
+FOUNDATION_EXPORT double SceneRendererVersionNumber;
+
+//! Project version string for SceneRenderer.
+FOUNDATION_EXPORT const unsigned char SceneRendererVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SceneRenderer/PublicHeader.h>
+
+
